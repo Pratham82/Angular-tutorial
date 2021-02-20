1bda252bf8cd7207ef719e3dbb299afc0cc69f05
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-02-20 18:11:30 +0530

    Added started project

---
+++ Angular-The-Complete-Guide/first-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-new-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Prathamesh\'s First Angular App'
+  name= 'Prathamesh'
+}
