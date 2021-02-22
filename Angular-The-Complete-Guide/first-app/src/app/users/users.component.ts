cfc3570c57f5edad77b16fc0611be38427039016
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-02-22 16:47:12 +0530

    Added newly created component using cli and manually

---
+++ Angular-The-Complete-Guide/first-app/src/app/users/users.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  //template: '<p>This is start of user component</p> <app-server></app-server>',
+  styleUrls: ['./users.component.css'],
+})
+export class UsersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
