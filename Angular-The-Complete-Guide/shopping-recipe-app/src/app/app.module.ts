e3a544fe73c7a0a99b9a14f045838108bd5666cb
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-02-27 18:39:45 +0530

    Added shopping recipe app

---
+++ Angular-The-Complete-Guide/shopping-recipe-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesItemComponent } from './recipes/recipe-list/recipes-item/recipes-item.component';
+import { RecipeDeatilsComponent } from './recipes/recipe-deatils/recipe-deatils.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipesItemComponent,
+    RecipeDeatilsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ Angular-The-Complete-Guide/shopping-recipe-app/src/app/recipes/recipe-deatils/recipe-deatils.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-deatils',
+  templateUrl: './recipe-deatils.component.html',
+  styleUrls: ['./recipe-deatils.component.css']
+})
+export class RecipeDeatilsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
