d836a7a2e82c2e1b8e3a0bef9baaa244e902bfed
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-02-27 17:19:15 +0530

    Updated app component

--- Angular-The-Complete-Guide/app-directives/src/app/app.module.ts
+++ Angular-The-Complete-Guide/app-directives/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ClientComponent } from './client2/cleint.component';
 import { UsersComponent } from './users/users.component';
+import { AssignmentComponent } from './assignment/assignment.component';
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ClientComponent,
     UsersComponent,
+    AssignmentComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
