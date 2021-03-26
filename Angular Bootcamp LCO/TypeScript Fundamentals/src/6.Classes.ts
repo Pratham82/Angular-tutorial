ec03d2c86314a3ba6be3e548f34e9afd3b29b402
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-03-26 23:08:13 +0530

    Updated class

--- Angular Bootcamp LCO/TypeScript Fundamentals/src/6.Classes.ts
+++ Angular Bootcamp LCO/TypeScript Fundamentals/src/6.Classes.ts
@@ -1,5 +1,26 @@
-class Employee {
-  constructor() { }
+interface Person {
+  name: string
+  checkThisOut(): void
+}
 
-  greet = (greeter: string) => console.log(`Hey ${greeter} !!!`)
+interface Emp {
+  empCode: number
+}
+
+// Classes implement interfaces which will make sure that data that is coming in is of proper type
+// If the data provided in a class does not follows the rules of interface then we will get an error at compile time
+class EmployeeClass implements Person, Emp {
+  name: string
+  empName: string
+  empCode: number = 0
+
+  constructor(empCode: number, empName: string, name: string) {
+    this.empName = empName
+    this.empCode = empCode
+    this.name = name
+  }
+
+  checkThisOut() {
+    console.log(`Hello there!!`)
+  }
 }
